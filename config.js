0185ea5effe44e00ec7b37a8868c14c9fdfa437d
rémy Lefoll <remy.lefoll98@gmail.com>
2020-04-19 18:24:25 +0200

    Moderniser le bot

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.TOKEN = 'NTcwMjk5NDM5MjM5OTIxNzk1.XpwyXA.MWCIklJypBgh3Ze3PhL1E7QgzQw'
+exports.PREFIX = '?'
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { Client } = require('discord.js')
-const client = new Client({ disableMentions: 'everyone' })
+const { TOKEN, PREFIX } = require('./config')
+const client = new Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!')
-  if (msg.content === 'everyone') msg.channel.send('@everyone, salut à tous !', { disableMentions : false })
-  if (msg.content === 'noteveryone') msg.channel.send('@everyone (noteveryone), salut à tous !')
+client.on('message', message => {
+  if (message.content.startsWith(`${PREFIX}salut`)) message.channel.send('Je suis le bot !')
+  if (message.content.startsWith(`${PREFIX}ping`)) message.channel.send('Pong!')
 })
 
-client.login('NTcwMjk5NDM5MjM5OTIxNzk1.XpsPVA.d8JWvC9HCcQ53hdzjJH8Ap4rXJA')
+client.login(TOKEN)
