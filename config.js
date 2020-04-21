4b1ac8d63d2f111e3c3b75c246daa1bafce30076
rémy Lefoll <remy.lefoll98@gmail.com>
2020-04-21 19:38:23 +0200

    Ajoute de commandes avec arguments

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.TOKEN = 'NTcwMjk5NDM5MjM5OTIxNzk1.XpwyXA.MWCIklJypBgh3Ze3PhL1E7QgzQw'
-exports.PREFIX = '?'
+exports.TOKEN = 'NTcwMjk5NDM5MjM5OTIxNzk1.Xp8qww.-UHLiPwqkgdp-7PyD14PIBNxP4A'
+exports.PREFIX = '!'
--- main.js
+++ main.js
@@ -7,8 +7,23 @@ client.on('ready', () => {
 })
 
 client.on('message', message => {
-  if (message.content.startsWith(`${PREFIX}salut`)) message.channel.send('Je suis le bot !')
-  if (message.content.startsWith(`${PREFIX}ping`)) message.channel.send('Pong!')
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return
+  const args = message.content.slice(PREFIX.length).split(/ +/)
+  console.log(args)
+  // [bla, bla, bla]
+  // ?user bla bla bla
+  const command = args.shift().toLocaleLowerCase()
+  // ?user
+  console.log(command)
+
+  if (command === 'salut') message.channel.send('Je suis le bot !')
+  if (command === 'ping') message.channel.send('Pong!')
+  if (command === 'server') message.channel.send(`Je suis sur le serveur ${message.guild.name}.`)
+  if (command === 'user') message.channel.send(`Je suis l'utilisateur ${message.author.tag}.`)
+  if (command === 'userinfo' ) {
+    const user_mention = message.mentions.users.first()
+    message.channel.send(`Voici le tag de la personne mentionné : ${user_mention.tag}`)
+  }
 })
 
 client.login(TOKEN)
