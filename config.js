58cbad1092415d90bfd1907f318c4847986af5e5
rémy Lefoll <remy.lefoll98@gmail.com>
2020-04-22 12:19:02 +0200

    Crétion du premier handler

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.TOKEN = 'NTcwMjk5NDM5MjM5OTIxNzk1.Xp8xAw.C0B-pcHcanjAhd2aaThKgwnCExQ'
+exports.PREFIX = '!'
--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const { Client } = require('discord.js')
+const fs = require("fs")
+const { Client, Collection } = require('discord.js')
 const { TOKEN, PREFIX } = require('./config')
+
 const client = new Client()
+client.commands = new Collection()
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+  console.log(client.commands)
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-})
+});
 
 client.on('message', message => {
-  if (!message.content.startsWith(PREFIX) || message.author.bot) return
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
   const args = message.content.slice(PREFIX.length).split(/ +/)
-  console.log(args)
-  // [bla, bla, bla]
-  // ?user bla bla bla
-  const command = args.shift().toLocaleLowerCase()
-  // ?user
-  console.log(command)
+  const command = args.shift().toLowerCase()
 
-  if (command === 'salut') message.channel.send('Je suis le bot !')
-  if (command === 'ping') message.channel.send('Pong!')
-  if (command === 'server') message.channel.send(`Je suis sur le serveur ${message.guild.name}.`)
-  if (command === 'user') message.channel.send(`Je suis l'utilisateur ${message.author.tag}.`)
-  if (command === 'userinfo' ) {
-    const user_mention = message.mentions.users.first()
-    message.channel.send(`Voici le tag de la personne mentionné : ${user_mention.tag}`)
-  }
+  if (!client.commands.has(command)) return
+  client.commands.get(command).execute(message, args)
 })
 
 client.login(TOKEN)
