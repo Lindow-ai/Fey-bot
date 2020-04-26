69dc3edd12c15d0a21c84a17c36daec7ebd34fe1
rémy Lefoll <remy.lefoll98@gmail.com>
2020-04-26 17:43:24 +0200

    Créations de plusieurs dossiers pour les commandes

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
-const fs = require("fs")
 const { Client, Collection } = require('discord.js')
+const { readdirSync } = require("fs")
 const { TOKEN, PREFIX } = require('./config')
 
 const client = new Client()
 client.commands = new Collection()
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const loadCommands = (dir = "./commands/") => {
+  readdirSync(dir).forEach(dirs => {
+    const commands = readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"))
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
-  client.commands.set(command.name, command)
-  console.log(`Commande chargée: ${command.name}`)
+    for (const file of commands) {
+      const getFileName = require(`${dir}/${dirs}/${file}`)
+      client.commands.set(getFileName.help.name, getFileName)
+      console.log(`Commande chargée: ${getFileName.help.name}`)
+    }
+  })
 }
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+loadCommands();
 
 client.on('message', message => {
-  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return
   const args = message.content.slice(PREFIX.length).split(/ +/)
   const command = args.shift().toLowerCase()
-
-  if (!client.commands.has(command)) return
-  client.commands.get(command).execute(client, message, args)
+  
+  if (!client.commands.has(command)) return;
+  client.commands.get(command).run(client, message, args)
 })
 
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`))
 client.login(TOKEN)
