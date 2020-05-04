535b51d3af0607d1b0e3d7b53f3c15c3ae3fc769
rémy Lefoll <remy.lefoll98@gmail.com>
2020-05-04 16:25:27 +0200

    Crétions de de logs pour les commandes

--- commands/Misc/valnuit.js
+++ commands/Misc/valnuit.js
@@ -1,9 +1,25 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports.run = (client, message, args) => {
     message.channel.send({files: [{
         attachment: './valnuit.mp3',
         name: 'valnuit.mp3'
     }]
 })
+    const embed = new MessageEmbed()
+      .setColor('bb0b0b')
+      .setTitle("Bienvenue à Valnuit !")
+      .setAuthor("Osiris")
+      .setDescription("Une communauté reculée mais amicale, où le Soleil est chaleureux, la Lune magnifique, et où de mystérieuses lumières passent au dessus de nos têtes quand nous faisons tous semblant de dormir.\n")
+      .setThumbnail('https://zupimages.net/up/20/17/k14d.png')
+      .addField("Ainsi, différents sujets restent à votre disposition",
+      "> Aucune phrase ajoutée.")
+      .addField("Au plaisir de vous retrouver sur notre serveur,",
+            "Le Conseil de Valnuit.") 
+      .setTimestamp()
+      .setFooter("Coded by Osiris");
+
+    message.channel.send(embed)
   }
   
   module.exports.help = {
--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -1,7 +1,19 @@
+const { MessageEmbed } = require('discord.js')
+
 module.exports.run = (client, message, args) => {
     const user = message.mentions.users.first() 
-    const reason = args.splice(1).join(' ')
+    const reason = (args.splice(1).join(' ') || 'Aucune raison spécifiée')
     user ? message.guild.member(user).ban(reason) : message.channel.send("L'utilisateur n'existe pas !")
+
+    const embed = new MessageEmbed()
+    .setAuthor(`${user.username} (${user.id})`)
+    .setColor("#dc143c")
+    .setDescription(`**Action**: ban\n**Raison**: ${reason}`)
+    .setThumbnail(user.avatarURL())
+    .setTimestamp()
+    .setFooter(message.author.username, message.author.avatarURL())
+
+    client.channels.cache.get('685819141524946954').send(embed)
   }
   
   module.exports.help = {
